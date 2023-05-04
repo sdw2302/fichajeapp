c254c2034285304697bc7398534c98410162ef63
sdw2302 <shadow2302ezg@gmail.com>
2023-05-04 10:37:33 +0200

    working on Schedules 1

--- src/main/java/com/mm/fichajeapp/SecondaryController.java
+++ src/main/java/com/mm/fichajeapp/SecondaryController.java
@@ -1,11 +1,10 @@
 package com.mm.fichajeapp;
 
-import java.io.IOException;
-
 import com.mm.fichajeapp.modelo.DataManagement;
 import com.mm.fichajeapp.modelo.Worker;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -13,7 +12,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 public class SecondaryController {
-     
+
     @FXML
     TextField NIF;
 
@@ -24,30 +23,39 @@ public class SecondaryController {
     TableView<Worker> tableWorkers;
 
     @FXML
-    TableColumn<Worker,String> DNI;
+    TableColumn<Worker, String> DNI;
     @FXML
-    TableColumn<Worker,String> Nombre;
+    TableColumn<Worker, String> Nombre;
     @FXML
-    TableColumn<Worker,String> Apellido;
+    TableColumn<Worker, String> Apellido;
     @FXML
-    TableColumn<Worker,Double> Horas_trabajadas;
+    TableColumn<Worker, Double> Horas_trabajadas;
+
+    @FXML
+    Button loadSchedules;
 
     DataManagement dm = new DataManagement();
 
     Worker w = new Worker(null, null, null, null);
 
-    public void initialize(){
+    public void initialize() {
 
         // ObservableList<Worker> trabajadores = dm.geTableAstList();
 
         ObservableList<Worker> listaTrabajadores = w.todosTrabajadores();
 
-        //DNI.setCellValueFactory(new PropertyValueFactory<>("dni_trabajador"));
+        // DNI.setCellValueFactory(new PropertyValueFactory<>("dni_trabajador"));
         DNI.setCellValueFactory(new PropertyValueFactory<Worker, String>("dni_trabajador"));
         Nombre.setCellValueFactory(new PropertyValueFactory<>("nombre_trabajador"));
         Apellido.setCellValueFactory(new PropertyValueFactory<>("apellido_trabajador"));
         Horas_trabajadas.setCellValueFactory(new PropertyValueFactory<>("horas_fichadas_trabajador"));
-        tableWorkers.setItems(listaTrabajadores);      
-        
+        tableWorkers.setItems(listaTrabajadores);
+
+    }
+
+    public void loadSchedulesClick() {
+        String nif = NIF.getText();
+
+        this.initialize();
     }
 }
--- src/main/java/com/mm/fichajeapp/modelo/DataManagement.java
+++ src/main/java/com/mm/fichajeapp/modelo/DataManagement.java
@@ -23,10 +23,31 @@ public class DataManagement {
                 trabajadores.add(new Worker(resultSet.getString(1), resultSet.getString(2), resultSet.getString(3),
                         resultSet.getDouble(4)));
 
+            conn.getConn().close();
         } catch (SQLException e) {
             System.out.println("Error: " + e.getMessage());
         }
         return trabajadores;
     }
 
+    public String loadSchedules(String dni) {
+        String toReturn = "";
+        String sql = "select id_horario, hora_inicio_horario, hora_final_horario from horario where id_horario = (select id_horario from horario_trabajador where id_trabajador = (select id_trabajador from trabajador where dni_trabajador = \""
+                + dni + "\"))";
+        DbConnection conn = new DbConnection();
+        try {
+            Statement ordre = conn.getConn().createStatement();
+            ResultSet resultSet = ordre.executeQuery(sql);
+            while (resultSet.next()) {
+                toReturn += resultSet.getString(1)
+                        + ": de " + resultSet.getString(2)
+                        + " a " + resultSet.getString(3) + ";";
+            }
+            conn.getConn().close();
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+            return "error";
+        }
+        return toReturn;
+    }
 }
