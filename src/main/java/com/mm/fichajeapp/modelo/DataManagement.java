df6415bec4aba9bc5816adeac896eeb86ec6144f
sdw2302 <shadow2302ezg@gmail.com>
2023-05-16 21:00:42 +0200

    started commenting code

--- src/main/java/com/mm/fichajeapp/modelo/DataManagement.java
+++ src/main/java/com/mm/fichajeapp/modelo/DataManagement.java
@@ -1,16 +1,14 @@
 package com.mm.fichajeapp.modelo;
 
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public class DataManagement {
 
-    public ObservableList<Worker> getTableWorkersAsList() {
+    public ObservableList<Worker> getTableWorkersAsList() { // get a list of all workers
 
         ObservableList<Worker> trabajadores = FXCollections.observableArrayList();
         String sql = "select dni_trabajador, nombre_trabajador, apellido_trabajador,  horas_fichadas_trabajador from trabajador";
@@ -18,8 +16,8 @@ public class DataManagement {
         DbConnection conn = new DbConnection();
         try {
             Statement ordre = conn.getConn().createStatement();
-            ResultSet resultSet = ordre.executeQuery(sql);
-            while (resultSet.next()) {
+            ResultSet resultSet = ordre.executeQuery(sql); // sends query from above to mysql server
+            while (resultSet.next()) { // for each result received creates a worker and adds to list
                 trabajadores.add(
                         new Worker(
                                 resultSet.getString(1),
@@ -28,78 +26,78 @@ public class DataManagement {
                                 resultSet.getDouble(4)));
             }
 
-        } catch (SQLException e) {
+        } catch (Exception e) { // in case of error
             System.out.println("Error: " + e.getMessage());
         }
-        return trabajadores;
+        return trabajadores; // returns all workers received as list of objects
     }
 
-    public String loadSchedules(String dni) {
+    public String loadSchedules(String dni) { // get all assigned schedules of a specific worker
         String toReturn = "";
         String sql = "select id_horario, hora_inicio_horario, hora_final_horario, tiempo_descanso_horario from horario where id_horario in (select id_horario from horario_trabajador where id_trabajador = (select id_trabajador from trabajador where dni_trabajador = \""
                 + dni + "\"))";
         DbConnection conn = new DbConnection();
         try {
             Statement ordre = conn.getConn().createStatement();
-            ResultSet resultSet = ordre.executeQuery(sql);
-            while (resultSet.next()) {
+            ResultSet resultSet = ordre.executeQuery(sql); // sends query from above to mysql server
+            while (resultSet.next()) { // for each result received adds Strings with schedule info to be divided
                 toReturn += resultSet.getString(1)
                         + ": de " + resultSet.getString(2)
                         + " a " + resultSet.getString(3)
                         + ", descanso: " + resultSet.getString(4) + ";";
             }
-        } catch (SQLException e) {
+        } catch (Exception e) { // in case of error
             System.out.println("Error: " + e.getMessage());
             return "";
         }
-        return toReturn;
+        return toReturn; // returns all schedules as one String with ; separator
     }
 
-    private int getAvailableSignTimeId() {
+    private int getAvailableSignTimeId() { // generates an id to be used in an insert
         String sql = "select id_fichaje from fichaje order by id_fichaje asc";
         DbConnection conn = new DbConnection();
         String toArray = "";
         try {
             Statement ordre = conn.getConn().createStatement();
-            ResultSet resultSet = ordre.executeQuery(sql);
-            while (resultSet.next()) {
+            ResultSet resultSet = ordre.executeQuery(sql); // sends query from above to mysql server
+            while (resultSet.next()) { // for each result received adds String with all ids
                 toArray += resultSet.getString(1) + ";";
             }
-        } catch (SQLException e) {
+        } catch (Exception e) { // in case of error
             System.out.println("Error: " + e.getMessage());
         }
-        if (toArray == "")
-            return 0;
-        String[] array = toArray.split(";");
-        int id = Integer.parseInt(array[array.length - 1]) + 1;
+        if (toArray == "") // if no ids returned
+            return 1;
+        String[] array = toArray.split(";"); // divides the ids into an array
+        int id = Integer.parseInt(array[array.length - 1]) + 1; // gets last id and adds 1 to return available id
         return id;
     }
 
-    public boolean signTime(String dni, int id_horario, String time) {
+    public boolean signTime(String dni, int id_horario, String time) { // inserts data into 'fichaje' table in db
         String sql = "select id_horario_trabajador from horario_trabajador where id_trabajador = (select id_trabajador from trabajador where dni_trabajador = '"
                 + dni + "') and id_horario = " + id_horario;
         DbConnection conn = new DbConnection();
         int id_horario_trabajador = 0;
         try {
             Statement ordre = conn.getConn().createStatement();
-            ResultSet resultSet = ordre.executeQuery(sql);
+            ResultSet resultSet = ordre.executeQuery(sql); // sends query from above to mysql server
             while (resultSet.next())
-                id_horario_trabajador = resultSet.getInt(1);
+                id_horario_trabajador = resultSet.getInt(1); // gets result
             sql = "insert into fichaje values (" + this.getAvailableSignTimeId() + ", "
-                    + String.valueOf(id_horario_trabajador) + ", " + time + ")";
+                    + String.valueOf(id_horario_trabajador) + ", " + time + ")"; // modifies the query
             try {
-                conn.getConn().createStatement().execute(sql);
-            } catch (Exception e) {
+                conn.getConn().createStatement().execute(sql); // sends query from above to mysql server
+            } catch (Exception e) { // in case of error
                 System.out.println("Error: " + e.getMessage());
             }
-        } catch (SQLException e) {
+        } catch (Exception e) { // in case of error
             System.out.println("Error: " + e.getMessage());
             return false;
         }
         return true;
     }
 
-    public ObservableList<Worker> getTableWorkersCompleteAsList() {
+    public ObservableList<Worker> getTableWorkersCompleteAsList() { // get a list of all workers (more data)
 
         ObservableList<Worker> trabajadores = FXCollections.observableArrayList();
         String sql = "select dni_trabajador, nombre_trabajador, apellido_trabajador, fecha_nacimiento_trabajador, nombre_empresa from trabajador, empresa where trabajador.id_empresa = empresa.id_empresa;";
@@ -118,13 +116,13 @@ public class DataManagement {
                                 resultSet.getString(5)));
             }
 
-        } catch (SQLException e) {
+        } catch (Exception e) { // in case of error
             System.out.println("Error: " + e.getMessage());
         }
         return trabajadores;
     }
 
-    public ObservableList<String> getNameCompanies() {
+    public ObservableList<String> getNameCompanies() { // get a list of names of all companies
 
         ObservableList<String> empresas = FXCollections.observableArrayList();
         String sql = "select nombre_empresa from empresa;";
@@ -138,7 +136,7 @@ public class DataManagement {
                         resultSet.getString(1));
             }
 
-        } catch (SQLException e) {
+        } catch (Exception e) { // in case of error
             System.out.println("Error: " + e.getMessage());
         }
         return empresas;
@@ -183,7 +181,7 @@ public class DataManagement {
                 System.out.println("Error: " + e.getMessage());
                 return false;
             }
-        } catch (SQLException e) {
+        } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
             return false;
         }
@@ -200,7 +198,7 @@ public class DataManagement {
             ResultSet resultSet = ordre.executeQuery(sql);
             while (resultSet.next())
                 toArray += resultSet.getString(1) + ";";
-        } catch (Exception e) {
+        } catch (Exception e) { // in case of error
             System.out.println("Error: " + e.getMessage());
         }
         if (toArray == "")
@@ -208,35 +206,6 @@ public class DataManagement {
         String[] array = toArray.split(";");
         int id = Integer.parseInt(array[array.length - 1]) + 1;
         return id;
-    }
-
-    public boolean checkDni(String DniToCheck) {
-
-        String dnis = "";
-        String sql = "select dni_trabajador from trabajador";
-
-        DbConnection conn = new DbConnection();
-
-        try {
-            Statement ordre = conn.getConn().createStatement();
-            ResultSet resultSet = ordre.executeQuery(sql);
-
-            while (resultSet.next()) {
-                dnis += ((resultSet.getString(1) + ";"));
-            }
-
-        } catch (SQLException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-
-        String[] dnisArray = dnis.split(";");
-
-        for (String dni : dnisArray) {
-            if (dni.equals(DniToCheck))
-                return false;
-        }
-
-        return true;
     }
 
     public ObservableList<Schedule> getTableSchedulesAsList() {
@@ -257,7 +226,7 @@ public class DataManagement {
                                 resultSet.getString(3),
                                 resultSet.getString(4)));
             }
-        } catch (Exception e) {
+        } catch (Exception e) { // in case of error
             System.out.println("Error: " + e.getMessage());
         }
         return schedules;
@@ -273,7 +242,7 @@ public class DataManagement {
             ResultSet resultSet = ordre.executeQuery(sql);
             while (resultSet.next())
                 toArray += resultSet.getString(1) + ";";
-        } catch (Exception e) {
+        } catch (Exception e) { // in case of error
             System.out.println("Error: " + e.getMessage());
         }
         if (toArray == "")
@@ -301,7 +270,7 @@ public class DataManagement {
             ResultSet resultSet = ordre.executeQuery(sql);
             while (resultSet.next())
                 toArray += resultSet.getString(1) + ";";
-        } catch (Exception e) {
+        } catch (Exception e) { // in case of error
             System.out.println("Error: " + e.getMessage());
         }
         if (toArray == "")
