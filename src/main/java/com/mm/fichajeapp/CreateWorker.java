dea4edcc5bcd9c435b5ea635fd781e4ae3a0964b
MikeChamp4 <zambranocmiguele@gmail.com>
2023-05-17 22:06:56 +0200

    CreateWorker Commented

--- src/main/java/com/mm/fichajeapp/CreateWorker.java
+++ src/main/java/com/mm/fichajeapp/CreateWorker.java
@@ -53,90 +53,121 @@ public class CreateWorker {
     DataManagement dm = new DataManagement();
 
     public void initialize() {
-
+        // Set cell value factories for the table columns
         DNI.setCellValueFactory(new PropertyValueFactory<>("dni_trabajador"));
         Nombre.setCellValueFactory(new PropertyValueFactory<>("nombre_trabajador"));
         Apellido.setCellValueFactory(new PropertyValueFactory<>("apellido_trabajador"));
         FechaNacimiento.setCellValueFactory(new PropertyValueFactory<>("fecha_nacimiento"));
         EmpresaResponsable.setCellValueFactory(new PropertyValueFactory<>("empresa_responsable"));
+        
+        // Set the items of the table with the complete list of workers from DataManagement
         tableWorkers.setItems(dm.getTableWorkersCompleteAsList());
-
+    
+        // Get the list of company names from DataManagement
         ObservableList<String> nombreEmpresa = dm.getNameCompanies();
         if (nombreEmpresa != null) {
-            for (String string : nombreEmpresa)
-                if (string != "")
+            // Add non-empty company names to the ComboBox
+            for (String string : nombreEmpresa) {
+                if (string != "") {
                     EmpresaResponsableToAdd.getItems().add(string);
+                }
+            }
         }
-
     }
+    
 
     public void switchToPrimary() throws IOException {
+        // Switches to the primary view.
         App.setRoot("primary");
     }
 
     public void switchToSecomdary() throws IOException {
+        // Switches to the secondary view.
         App.setRoot("secondary");
     }
 
     public void switchToCreateWorker() throws IOException {
+        // Switches to the CreateWorker view.
         App.setRoot("createWorker");
     }
 
     public void switchToScheduleManagement() throws IOException {
+        // Switches to the ScheduleManagement view.
         App.setRoot("scheduleManagement");
     }
 
     public void createWorker() {
+        // Check if any required field is empty
         if (NIFToAdd.getText().equals("") || NombreToAdd.getText().equals("") || ApellidoToAdd.getText().equals("")
                 || FechaNacimientoToAdd.getValue() == null
                 || EmpresaResponsableToAdd.getSelectionModel().getSelectedItem() == null) {
-            this.createAlert("Faltan datos");
+            this.createAlert("Faltan datos"); // Display an error alert for missing data
             return;
         }
+    
+        // Create the worker using the provided information
         boolean created = dm.createWorker(NIFToAdd.getText(), NombreToAdd.getText(), ApellidoToAdd.getText(),
                 FechaNacimientoToAdd.getValue(), EmpresaResponsableToAdd.getSelectionModel().getSelectedItem());
+        
+        // Check if the worker was successfully created
         if (!created)
-            this.createAlert("Ha ocurrido un error de servidor");
+            this.createAlert("Ha ocurrido un error de servidor"); // Display an error alert for server error
+    
+        // Clear and refresh the table of workers
         tableWorkers.getItems().clear();
         tableWorkers.setItems(dm.getTableWorkersCompleteAsList());
     }
-
+    
     public void deleteWorker() {
+        // Check if a worker is selected
         if (!this.checkIfSelected("Seleccione un trabajador"))
             return;
+        
+        // Delete the selected worker
         boolean deleted = dm.deleteWorker(tableWorkers.getSelectionModel().getSelectedItem());
+        
+        // Check if the worker was successfully deleted
         if (!deleted)
-            this.createAlert("Ha ocurrido un error de servidor");
+            this.createAlert("Ha ocurrido un error de servidor"); // Display an error alert for server error
+        
+        // Clear and refresh the table of workers
         tableWorkers.getItems().clear();
         tableWorkers.setItems(dm.getTableWorkersCompleteAsList());
     }
-
+    
     private void createAlert(String message) {
+        // Create an error alert with the given message
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Error");
         alert.setHeaderText("Ha ocurrido un error");
         alert.setContentText(message);
         alert.showAndWait();
-
     }
-
+    
     private boolean checkIfSelected(String msg) {
+        // Check if a worker is selected
         if (tableWorkers.getSelectionModel().getSelectedItem() == null) {
-            this.createAlert(msg);
+            this.createAlert(msg); // Display an error alert with the given message
             return false;
         }
         return true;
     }
+    
 
     public void modifyWorker() {
+        // Check if a worker is selected
         if (!this.checkIfSelected("Seleccione un trabajador"))
             return;
+        
+        // Check if any modification data is provided
         if (NIFToAdd.getText().isEmpty() && NombreToAdd.getText().isEmpty() && ApellidoToAdd.getText().isEmpty()
                 && FechaNacimientoToAdd.getValue() == null
                 && EmpresaResponsableToAdd.getSelectionModel().getSelectedItem() == null) {
             this.createAlert("Rellene o seleccione al menos un dato para modificar el trabajador");
             return;
         }
+        
+        // Modify the worker with the provided data
         if (!dm.modifyWorker(
                 NombreToAdd.getText().isEmpty()
                         ? tableWorkers.getSelectionModel().getSelectedItem().getNombre_trabajador()
@@ -153,9 +184,11 @@ public class CreateWorker {
                         ? tableWorkers.getSelectionModel().getSelectedItem().getEmpresa_responsable()
                         : EmpresaResponsableToAdd.getSelectionModel().getSelectedItem(),
                 tableWorkers.getSelectionModel().getSelectedItem().getDni_trabajador()))
-            this.createAlert("Ha ocurrido un error de servidor");
-
+            this.createAlert("Ha ocurrido un error de servidor"); // Display an error alert for server error
+    
+        // Clear and refresh the table of workers
         tableWorkers.getItems().clear();
         tableWorkers.setItems(dm.getTableWorkersCompleteAsList());
     }
+    
 }
